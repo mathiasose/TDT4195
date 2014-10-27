8054f298f0c27c52a1f27d4dcba4cf5942b453bf
Øyvind Robertsen <oyvind@arcticstyle.net>
2014-10-27 14:46:23 +0100

    add extra requirements, remove output images, update task3 solution

--- visual_computing/LAB1/flatfield.py
+++ visual_computing/LAB1/flatfield.py
@@ -2,22 +2,21 @@ from __future__ import print_function, division
 
 import os
 
+import matplotlib.pyplot as pl
+import matplotlib.cm as cm
+import numpy
 from scipy.misc.pilutil import imread, imsave
 
 OUTPUT_DIR = 'output/'
 
 
 def normalize_bw(img_matrix):
-    img_matrix = map(lambda row: map(lambda val: round(float(val) / 255.0, 2), row), img_matrix)
+    img_matrix = map(lambda row: map(lambda val: float(val) / 255.0, row), img_matrix)
     return img_matrix
 
 
 def correct_with_flatfield(img, flatfield):
-    for a_row, b_row in zip(img, flatfield):
-        for a_px, b_px in zip(a_row, b_row):
-            a_px /= b_px
-
-    return img
+    return numpy.divide(img, flatfield)
 
 
 def main():
@@ -27,10 +26,12 @@ def main():
     nasa_matrix = normalize_bw(imread(NASA))
     flatfield_matrix = normalize_bw(imread(FLATFIELD))
 
-    correct_with_flatfield(nasa_matrix, flatfield_matrix)
+    corrected_image = correct_with_flatfield(nasa_matrix, flatfield_matrix)
 
     output_path = os.path.join(OUTPUT_DIR, os.path.split(NASA)[-1])
-    imsave(output_path, nasa_matrix)
+    imsave(output_path, corrected_image)
+    pl.imshow(corrected_image, cmap=cm.Greys_r)
+    pl.show()
 
 
 main()
