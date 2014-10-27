20c7bdb618e432aae02ff3790afae0643d90c87e
Øyvind Robertsen <oyvind@arcticstyle.net>
2014-10-27 17:43:27 +0100

    complete task5

--- visual_computing/LAB1/noise.py
+++ visual_computing/LAB1/noise.py
@@ -3,18 +3,31 @@
 import matplotlib.pyplot as pl
 import matplotlib.cm as cm
 import numpy.random
-from scipy.misc.pilutil import imread
+from copy import deepcopy
+from scipy.misc import lena
+from scipy.misc.pilutil import imsave
 from flatfield import normalize_bw
 
 
+def salt_and_pepper_noise(image, density):
+    ret = deepcopy(image)
+    for y in range(len(image)):
+        for x in range(len(image[0])):
+            rnd = numpy.random.random()
+            if rnd < density:
+                ret[y][x] = numpy.random.randint(2)
+    return ret
+
+
 def main():
-    IMG = 'img/task5.png'
-
-    img_mat = imread(IMG)
-    print(img_mat)
-    noisy = img_mat + numpy.random.random(img_mat.shape)
-
-    pl.imshow(noisy, cmap=cm.Greys_r)
+    img_mat = numpy.asarray(normalize_bw(lena()))
+    noisy_0_1 = salt_and_pepper_noise(img_mat, 0.1)
+    imsave('output/noisy_0_1.png', noisy_0_1)
+    noisy_0_3 = salt_and_pepper_noise(img_mat, 0.3)
+    imsave('output/noisy_0_3.png', noisy_0_3)
+    noisy_0_8 = salt_and_pepper_noise(img_mat, 0.8)
+    imsave('output/noisy_0_8.png', noisy_0_8)
+    pl.imshow(noisy_0_1, cmap=cm.Greys_r)
     pl.show()
 
 if __name__ == "__main__":
