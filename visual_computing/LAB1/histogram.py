7163370059c64a3ae17569d8d1448c2437673beb
Mathias Ose <mathias.ose@gmail.com>
2014-10-29 15:59:03 +0100

    rest of histogram task

--- visual_computing/LAB1/histogram.py
+++ visual_computing/LAB1/histogram.py
@@ -1,14 +1,15 @@
-from __future__ import print_function, division
+#! /usr/bin/python
 
+from __future__ import print_function, division
+from collections import defaultdict
 import os
 
 import numpy as np
 import matplotlib.pyplot as pl
 import matplotlib.cm as cm
-from scipy.misc.pilutil import imsave
+from scipy.misc.pilutil import imsave, imread
 
 from gamma import pixel_wise_transform
-from gamma import minmax_2d
 
 
 OUTPUT_DIR = 'output/'
@@ -21,16 +22,13 @@ def random_matrix(w, h, n):
     return np.random.randint(n, size=(h, w))
 
 
-def n_count(matrix, n):
-    '''
-    :returns the number of occurences of n in the 2d matrix
-    '''
-    return sum(sum(x == n for x in row) for row in matrix)
-
-
 def create_histogram(matrix):
-    m_min, m_max = minmax_2d(matrix)
-    return dict((n, n_count(matrix, n)) for n in range(m_min, m_max + 1))
+    histogram = defaultdict(int)
+    for row in matrix:
+        for px in row:
+            histogram[px] += 1
+
+    return histogram
 
 
 def normalize_histogram(histogram):
@@ -56,15 +54,15 @@ def main():
     cdf = create_cdf(normalized)
     image = pixel_wise_transform(matrix, cdf)
 
-    print(matrix)
-    print(histogram)
-    print(normalized)
-    print(image)
-
     output_path = os.path.join(OUTPUT_DIR, 'histogram.png')
     imsave(output_path, image)
     pl.imshow(image, cmap=cm.Greys_r)
     pl.show()
+
+    EINSTEIN = 'img/einstein_lowcontrast.png'
+    einstein = imread(EINSTEIN)
+    ein_cdf = create_cdf(normalize_histogram(create_histogram(einstein)))
+    imsave(os.path.join(OUTPUT_DIR, "cdf_" + os.path.split(EINSTEIN)[-1]), pixel_wise_transform(einstein, ein_cdf))
 
 
 if __name__ == "__main__":
