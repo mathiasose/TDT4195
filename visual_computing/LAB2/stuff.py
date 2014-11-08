1f8bd06b20c1ab1052165628f8bfa4891c436326
Mathias Ose <mathias.ose@gmail.com>
2014-11-08 20:59:25 +0100

    2.3 stuff

--- visual_computing/LAB2/stuff.py
+++ visual_computing/LAB2/stuff.py
@@ -1,9 +1,13 @@
+from __future__ import division, print_function
+
 from numpy.core.numeric import Inf
 from numpy.random.mtrand import normal
 from scipy.integrate.quadpack import quad
 import numpy as np
+from scipy.ndimage.filters import gaussian_filter
+from math import pi
 
-from LAB1.image_manipulation import transform
+from LAB1.image_manipulation import transform, alias
 
 
 def list_get(li, i, pad_with=0):
@@ -24,8 +28,8 @@ def truncate_range(val, lo=0, hi=1):
 def get_neighbourhood(matrix, x, y, d=1):
     xs = xrange(x - d, x + d)
     ys = xrange(y - d, y + d)
-    neighbours = [matrix_get(matrix, i, j, pad_with=-1) for i in xs for j in ys]
-    neighbours = filter(lambda n: n != -1, neighbours)
+    neighbours = [matrix_get(matrix, i, j, pad_with=None) for i in xs for j in ys]
+    neighbours = filter(lambda n: n is not None, neighbours)
     return neighbours
 
 
@@ -37,13 +41,15 @@ def median(li):
     li = sorted(li)
     m = len(li) // 2
     if len(li) % 2 == 0:
-        return average([li[m], li[m+1]])
+        return average([li[m], li[m + 1]])
 
     return li[m]
 
 
 def transform_by_coordinate(matrix, fun):
-    return np.asarray([[fun(x, y) for x, px in enumerate(row)] for y, row in enumerate(matrix)])
+    return np.asarray([[fun(x, y) for x, _ in enumerate(row)] for y, row in enumerate(matrix)])
+
+
 
 
 # Task 1
@@ -52,16 +58,11 @@ def discrete_convolution(f, g, D):
 
 
 def continuous_convolution(f, g):
-    return lambda t: round(quad(lambda v: (f(v) * g(t - v)), -Inf, Inf)[0], 1)
+    return lambda t: quad(lambda v: (f(v) * g(t - v)), -Inf, Inf)[0]
 
 
 # Task 2
-def gaussian_noise(matrix, mean, std):
-    """
-    :returns the matrix with some noise randomly added.
-    Each noisy pixel will randomly be either black or white.
-    The amount of noise increases with the parameter density
-    """
+def gaussian_noise(matrix, mean=0, std=0.1):
     return transform(matrix, lambda px: truncate_range(px + normal(loc=mean, scale=std)))
 
 
@@ -81,3 +82,24 @@ def median_mask(matrix, filter_size=3):
     d = filter_size // 2
     f = lambda x, y: median(get_neighbourhood(matrix, x, y, d=d))
     return transform_by_coordinate(matrix, f)
+
+
+def convolution_2d(f, g, D_x, D_y):
+    return lambda x, y: sum(sum(f(x, y) * g(n_x, n_y) for n_x in D_x) for n_y in D_y)
+
+
+def gauss_kernel(size, sigma):
+    x, y = np.mgrid[-size:size + 1, -size:size + 1]
+    g = (1 / (2 * pi * sigma**2)) * np.exp(-(x ** 2 / size + y ** 2 / size) / (2 * sigma**2))
+    return g / g.sum()
+
+
+def gauss_filter(matrix, sigma):
+    # kernel = gauss_kernel(2, sigma)
+    # f = lambda x, y: matrix_get(matrix, x, y)
+    # g = lambda x, y: matrix_get(kernel, x, y)
+    # # r = len(kernel) // 2
+    # # D = range(-r, r)  # D_1 == D_2 for square pictures
+    # return transform_by_coordinate(matrix, convolution_2d(f, g, range(len(kernel[0])), range(len(kernel))))
+
+    return gaussian_filter(matrix, sigma)
--- visual_computing/LAB2/tasks.py
+++ visual_computing/LAB2/tasks.py
@@ -1,23 +1,28 @@
 from __future__ import print_function, division
+import os
+
 from numpy import linspace
 from numpy import log
 from numpy.fft import fft2, fftshift, ifft2
-from LAB1.image_manipulation import normalize_intensity, salt_and_pepper_noise
-from LAB2.stuff import discrete_convolution, list_get, continuous_convolution, gaussian_noise, averaging_mask
-
-import os
-
 from scipy.misc.pilutil import imread, imsave
+from scipy.ndimage.filters import gaussian_filter
 
-OUTPUT_DIR = os.path.join(os.path.dirname(__file__), 'output/')
-CAMERAMAN = os.path.join(os.path.dirname(__file__), 'img/cameraman.png')
-BRICKS = os.path.join(os.path.dirname(__file__), 'img/bricks_bw.png')
-HIGH_PASS = os.path.join(os.path.dirname(__file__), 'img/high_pass.png')
-LOW_PASS = os.path.join(os.path.dirname(__file__), 'img/low_pass.png')
-BRICKS_2 = os.path.join(os.path.dirname(__file__), 'img/bricks_bw_256.png')
+from LAB1.image_manipulation import normalize_intensity, salt_and_pepper_noise, alias
+from LAB2.stuff import discrete_convolution, list_get, continuous_convolution, gaussian_noise, averaging_mask, \
+    gauss_filter, gauss_kernel
+
+
+DIR = os.path.dirname(__file__)
+OUTPUT_DIR = os.path.join(DIR, 'output/')
+CAMERAMAN = os.path.join(DIR, 'img/cameraman.png')
+BRICKS = os.path.join(DIR, 'img/bricks_bw.png')
+HIGH_PASS = os.path.join(DIR, 'img/high_pass.png')
+LOW_PASS = os.path.join(DIR, 'img/low_pass.png')
+BRICKS_2 = os.path.join(DIR, 'img/bricks_bw_256.png')
 
 
 def task1_1():
+    print('1.1')
     F = [1, 1, 1, 1, 1]
     G = [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0]
     f = lambda n: list_get(F, n)
@@ -30,13 +35,17 @@ def task1_1():
 
 
 def task1_2():
-    f = lambda t: 0 if abs(t) > .5 else 1
-    g = f
+    print('1.2')
+    f = g = lambda t: 0 if abs(t) > .5 else 1
 
-    print(map(continuous_convolution(f, g), linspace(-1, 1, 9)))
+    values = linspace(-1, 1, 9)
+    convoluted = map(continuous_convolution(f, g), values)
+    rounded = map(lambda x: round(x, 1), convoluted)
+    print(zip(values, rounded))
 
 
 def task2_1():
+    print('2.1')
     img = normalize_intensity(imread(CAMERAMAN))
     gaussian = gaussian_noise(img, mean=0, std=0.1)
     peppered = salt_and_pepper_noise(img, density=0.25)
@@ -49,6 +58,7 @@ def task2_1():
 
 
 def task2_2():
+    print('2.2')
     img = normalize_intensity(imread(CAMERAMAN))
     gaussian = gaussian_noise(img, mean=0, std=0.1)
     peppered = salt_and_pepper_noise(img, density=0.25)
@@ -60,7 +70,19 @@ def task2_2():
     imsave(output_path, averaging_mask(peppered, filter_size=5))
 
 
+def task2_3():
+    print('2.3')
+    img = normalize_intensity(imread(BRICKS))
+
+    img = gauss_filter(img, 0.8)
+    img = alias(img, 4)
+
+    output_path = os.path.join(OUTPUT_DIR, "2_3_" + os.path.split(BRICKS)[-1])
+    imsave(output_path, img)
+
+
 def task3_1():
+    print('3.1')
     c_img = normalize_intensity(imread(CAMERAMAN))
     fft_res = fft2(a=c_img)
     output_path = os.path.join(OUTPUT_DIR, "3_1_fft_spectrum_" + os.path.split(CAMERAMAN)[-1])
@@ -73,6 +95,7 @@ def task3_1():
 
 
 def task3_2():
+    print('3.2')
     low_pass = normalize_intensity(imread(LOW_PASS))
     high_pass = normalize_intensity(imread(HIGH_PASS))
 
@@ -87,9 +110,10 @@ def task3_2():
     output_path = os.path.join(OUTPUT_DIR, "3_2_high_pass_" + os.path.split(BRICKS_2)[-1])
 
 
-#task1_1()
-#task1_2()
-#task2_1()
-#task2_2()
-#task3_1()
-task3_2()
+# task1_1()
+# task1_2()
+# task2_1()
+# task2_2()
+task2_3()
+# task3_1()
+# task3_2()
